da51cfe2a745b1db8d0f387e96afe5eb6a8f245c
Mark Cooke <mark@creativecookies.net>
2018-08-01 13:15:44 +0930

    rewrote the serial output stream for better clarity. signal a read to both ambiant and active sensor measurements instead of calling them separately.

--- Examples/ReadSensors/ReadSensors.ino
+++ Examples/ReadSensors/ReadSensors.ino
@@ -6,32 +6,32 @@ PulsePlug<> pulse(Wire);
 void setup()
 {
    Serial.begin(9600);
-   Serial.println("pulse_PSO2");
+   Serial.println(__FILE__);
 
    while (pulse.isPresent() == false)
    {
-      Serial.println("No SI114x found");
+      Serial.println(F("No SI114x found"));
       delay(1000);
    }
    
-   Serial.println("SI114x Pulse Sensor found");
+   Serial.println(F("SI114x Pulse Sensor found"));
    pulse.init();
 }
 
 void loop()
 {
-   pulse.readSensor(1);   // sensorIdx = 1(HeartRate),2(Ambiant),3(Both)
-   Serial.print("Active LEDs : ");
+   pulse.readSensor(3);   // sensorIdx = 1(HeartRate),2(Ambiant),3(Both)
+   Serial.print(F("Active LEDs [red, ir1, ir2] = ["));
    Serial.print(pulse.led_red); 
-   Serial.print(",");
+   Serial.print(F(", "));
    Serial.print(pulse.led_ir1);
-   Serial.print(",");
-   Serial.println(pulse.led_ir2);
-   delay(500);
-   pulse.readSensor(2);   // sensorIdx = 1(HeartRate),2(Ambiant),3(Both)
-   Serial.print("Ambiant LEDs : ");
+   Serial.print(F(", "));
+   Serial.print(pulse.led_ir2);
+   Serial.println(F("]"));
+   Serial.print(F("Ambiant LEDs [visible, ir] = ["));
+   Serial.print(pulse.als_vis);
+   Serial.print(F(", "));
    Serial.print(pulse.als_ir); 
-   Serial.print(",");
-   Serial.println(pulse.als_vis);
-   delay(500);
+   Serial.println(F("]"));
+   delay(1000);
 }
